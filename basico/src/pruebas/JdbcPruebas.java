3c0ac9f9e7b574f0f5998b2b780dbf05bade60fd
JavierLete <JavierLete@PC-JAVIER>
2026-03-26 17:26:43 +0100

    Ejemplo con PreparedStatement

--- basico/src/pruebas/JdbcPruebas.java
+++ basico/src/pruebas/JdbcPruebas.java
@@ -1,6 +1,7 @@
 package pruebas;
 
 import java.sql.*;
+import java.time.LocalDate;
 
 public class JdbcPruebas {
 	public static void main(String[] args) throws SQLException {
@@ -11,14 +12,32 @@ public class JdbcPruebas {
 		Connection con = DriverManager.getConnection(url, usuario, password);
 		Statement st = con.createStatement();
 
-		System.out.println(
-				st.executeUpdate("INSERT INTO personas (nombre, fecha_nacimiento) VALUES ('Juan', '2000-01-02')"));
+//		System.out.println(
+//				st.executeUpdate("INSERT INTO personas (nombre, fecha_nacimiento) VALUES ('Juan', '2000-01-02')"));
+
+		String nombreIntroducido = "María"; // "Robert', ''); DROP TABLE personas; --";
+		LocalDate fechaNacimientoIntroducida = LocalDate.of(2001, 2, 3);
+
+		String sql = "INSERT INTO personas (nombre, fecha_nacimiento) VALUES (?,?)";
+
+		PreparedStatement pst = con.prepareStatement(sql);
+
+		System.out.println(sql);
+
+		pst.setString(1, nombreIntroducido);
+		pst.setDate(2, java.sql.Date.valueOf(fechaNacimientoIntroducida));
+
+		System.out.println(pst.executeUpdate());
 
 		ResultSet rs = st.executeQuery("SELECT * FROM personas");
 
 		while (rs.next()) {
-			System.out.printf("%5s %-20s %s\n", rs.getString("id"), rs.getString("nombre"),
-					rs.getString("fecha_nacimiento"));
+			String id = rs.getString("id");
+			String nombre = rs.getString("nombre");
+			java.sql.Date fechaNacimiento = rs.getDate("fecha_nacimiento");
+
+			System.out.printf("%5s %-20s %s\n", id, nombre,
+					fechaNacimiento != null ? fechaNacimiento.toLocalDate() : "");
 		}
 
 	}
