cf936d1bc3c2ae8098f366a1cf6f8089012b37cf
JavierLete <JavierLete@PC-JAVIER>
2026-03-30 16:26:06 +0200

    Refactorización de DaoPersonaSqlite

--- basico/src/accesodatos/DaoPersonaSqlite.java
+++ basico/src/accesodatos/DaoPersonaSqlite.java
@@ -19,114 +19,76 @@ public class DaoPersonaSqlite implements DaoPersona {
 
 	@Override
 	public Iterable<Persona> obtenerTodos() {
-		Collection<Persona> personas = new HashSet<>();
-
-		try (Connection con = DriverManager.getConnection(URL, USER, PASS);
-				PreparedStatement pst = con.prepareStatement("SELECT * FROM personas");
-				ResultSet rs = pst.executeQuery()) {
-			while (rs.next()) {
-				Long id = rs.getLong("id");
-				String nombre = rs.getString("nombre");
-				java.sql.Date fechaNacimientoDate = rs.getDate("fecha_nacimiento");
-				LocalDate fechaNacimiento = fechaNacimientoDate != null ? fechaNacimientoDate.toLocalDate() : null;
-
-				Persona persona = new Persona(id, nombre, fechaNacimiento);
-
-				personas.add(persona);
-			}
-
-			return personas;
-		} catch (SQLException e) {
-			throw new DaoException("Error en la capa DAO", e);
-		}
+		return ejecutarSql("SELECT * FROM personas", null);
 	}
 
 	@Override
 	public Persona obtenerPorId(Long id) {
-		try (Connection con = DriverManager.getConnection(URL, USER, PASS);
-				PreparedStatement pst = con.prepareStatement("SELECT * FROM personas WHERE id=?");) {
-			pst.setLong(1, id);
-
-			try (ResultSet rs = pst.executeQuery()) {
-				if (rs.next()) {
-					String nombre = rs.getString("nombre");
-					java.sql.Date fechaNacimientoDate = rs.getDate("fecha_nacimiento");
-					LocalDate fechaNacimiento = fechaNacimientoDate != null ? fechaNacimientoDate.toLocalDate() : null;
-
-					return new Persona(id, nombre, fechaNacimiento);
-				}
-
-				return null;
-			}
-		} catch (SQLException e) {
-			throw new DaoException("Error en la capa DAO", e);
-		}
+		return ejecutarSqlUno("SELECT * FROM personas WHERE id=?", null, id);
 	}
 
 	@Override
 	public Persona insertar(Persona persona) {
-		try (Connection con = DriverManager.getConnection(URL, USER, PASS);
-				PreparedStatement pst = con
-						.prepareStatement("INSERT INTO personas (nombre, fecha_nacimiento) VALUES (?,?)");) {
-			pst.setString(1, persona.getNombre());
-			pst.setDate(2,
-					persona.getFechaNacimiento() != null ? java.sql.Date.valueOf(persona.getFechaNacimiento()) : null);
-
-			pst.executeUpdate();
-
-			return persona;
-		} catch (SQLException e) {
-			throw new DaoException("Error en la capa DAO", e);
-		}
+		return ejecutarSqlUno("INSERT INTO personas (nombre, fecha_nacimiento) VALUES (?,?)", persona,
+				persona.getNombre(),
+				persona.getFechaNacimiento() != null ? java.sql.Date.valueOf(persona.getFechaNacimiento()) : null);
 	}
 
 	@Override
 	public Persona modificar(Persona persona) {
-		try (Connection con = DriverManager.getConnection(URL, USER, PASS);
-				PreparedStatement pst = con
-						.prepareStatement("UPDATE personas SET nombre=?, fecha_nacimiento=? WHERE id=?");) {
-			pst.setString(1, persona.getNombre());
-			pst.setDate(2,
-					persona.getFechaNacimiento() != null ? java.sql.Date.valueOf(persona.getFechaNacimiento()) : null);
-			pst.setLong(3, persona.getId());
-
-			pst.executeUpdate();
-
-			return persona;
-		} catch (SQLException e) {
-			throw new DaoException("Error en la capa DAO", e);
-		}
+		return ejecutarSqlUno("UPDATE personas SET nombre=?, fecha_nacimiento=? WHERE id=?", persona,
+				persona.getNombre(),
+				persona.getFechaNacimiento() != null ? java.sql.Date.valueOf(persona.getFechaNacimiento()) : null,
+				persona.getId());
 	}
 
 	@Override
 	public void borrar(Long id) {
-		try (Connection con = DriverManager.getConnection(URL, USER, PASS);
-				PreparedStatement pst = con.prepareStatement("DELETE FROM personas WHERE id=?");) {
-			pst.setLong(1, id);
-
-			pst.executeUpdate();
-		} catch (SQLException e) {
-			throw new DaoException("Error en la capa DAO", e);
-		}
+		ejecutarSql("DELETE FROM personas WHERE id=?", null, id);
 	}
 
 	@Override
 	public Iterable<Persona> buscarPorNombre(String nombreParcial) {
+		return ejecutarSql("SELECT * FROM personas WHERE nombre LIKE ?", null, nombreParcial);
+	}
+
+	public Persona ejecutarSqlUno(String sql, Persona persona, Object... args) {
+		Iterable<Persona> personas = ejecutarSql(sql, persona, args);
+
+		if (personas.iterator().hasNext()) {
+			return personas.iterator().next();
+		}
+
+		return null;
+	}
+
+	public Iterable<Persona> ejecutarSql(String sql, Persona persona, Object... args) {
 		Collection<Persona> personas = new HashSet<>();
 
 		try (Connection con = DriverManager.getConnection(URL, USER, PASS);
-				PreparedStatement pst = con.prepareStatement("SELECT * FROM personas WHERE nombre LIKE ?");) {
-			pst.setString(1, "%" + nombreParcial + "%");
+				PreparedStatement pst = con.prepareStatement(sql);) {
+			for (int i = 0; i < args.length; i++) {
+				pst.setObject(i + 1, args[i]);
+			}
 
-			try (ResultSet rs = pst.executeQuery()) {
-				while (rs.next()) {
-					Long id = rs.getLong("id");
-					String nombre = rs.getString("nombre");
-					java.sql.Date fechaNacimientoDate = rs.getDate("fecha_nacimiento");
-					LocalDate fechaNacimiento = fechaNacimientoDate != null ? fechaNacimientoDate.toLocalDate() : null;
+			if (sql.trim().startsWith("SELECT")) {
+				try (ResultSet rs = pst.executeQuery()) {
+					while (rs.next()) {
+						Long id = rs.getLong("id");
+						String nombre = rs.getString("nombre");
+						java.sql.Date fechaNacimientoDate = rs.getDate("fecha_nacimiento");
+						LocalDate fechaNacimiento = fechaNacimientoDate != null ? fechaNacimientoDate.toLocalDate()
+								: null;
 
-					personas.add(new Persona(id, nombre, fechaNacimiento));
+						personas.add(new Persona(id, nombre, fechaNacimiento));
+					}
+
+					return personas;
 				}
+			} else {
+				pst.executeUpdate();
+
+				personas.add(persona);
 
 				return personas;
 			}
