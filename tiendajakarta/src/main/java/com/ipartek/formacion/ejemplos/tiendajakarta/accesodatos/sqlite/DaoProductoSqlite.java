3d28d5b90af0c1f9e8b0d4a0663733da50d12cb7
JavierLete <JavierLete@PC-JAVIER>
2026-04-16 17:14:31 +0200

    AccesoDatos: DaoProductoSqlite, tiendajakarta.db y configuraciones
    tiendajakarta.db debe estar dentro del directorio de eclipse para las
    pruebas

---
+++ tiendajakarta/src/main/java/com/ipartek/formacion/ejemplos/tiendajakarta/accesodatos/sqlite/DaoProductoSqlite.java
@@ -0,0 +1,35 @@
+package com.ipartek.formacion.ejemplos.tiendajakarta.accesodatos.sqlite;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.ipartek.formacion.ejemplos.tiendajakarta.accesodatos.DaoProducto;
+import com.ipartek.formacion.ejemplos.tiendajakarta.modelos.Producto;
+
+import bibliotecas.accesodatos.DaoException;
+import bibliotecas.accesodatos.DaoGenericoJdbc;
+
+public class DaoProductoSqlite extends DaoGenericoJdbc<Producto> implements DaoProducto {
+
+	public DaoProductoSqlite() {
+		super("productos", new String[] { "nombre", "descripcion", "precio" }, DaoProductoSqlite::filaAObjeto,
+				DaoProductoSqlite::objetoAFila);
+	}
+
+	private static Producto filaAObjeto(ResultSet rs) {
+		try {
+			return new Producto(rs.getLong("id"), rs.getString("nombre"), rs.getString("descripcion"),
+					rs.getBigDecimal("precio"));
+		} catch (SQLException e) {
+			throw new DaoException("No he podido convertir la fila a objeto", e);
+		}
+	}
+
+	private static Object[] objetoAFila(Producto p) {
+		if (p.getId() == null) {
+			return new Object[] { p.getNombre(), p.getDescripcion(), p.getPrecio() };
+		} else {
+			return new Object[] { p.getId(), p.getNombre(), p.getDescripcion(), p.getPrecio() };
+		}
+	}
+}
