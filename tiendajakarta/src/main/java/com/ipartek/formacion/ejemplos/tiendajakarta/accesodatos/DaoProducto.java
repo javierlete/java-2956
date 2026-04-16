1163a36d35143edf399776f285db2e7f0a061faa
JavierLete <JavierLete@PC-JAVIER>
2026-04-16 17:10:14 +0200

    AccesoDatos: interface DaoProducto

---
+++ tiendajakarta/src/main/java/com/ipartek/formacion/ejemplos/tiendajakarta/accesodatos/DaoProducto.java
@@ -0,0 +1,9 @@
+package com.ipartek.formacion.ejemplos.tiendajakarta.accesodatos;
+
+import com.ipartek.formacion.ejemplos.tiendajakarta.modelos.Producto;
+
+import bibliotecas.accesodatos.Dao;
+
+public interface DaoProducto extends Dao<Producto> {
+
+}
