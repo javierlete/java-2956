1c17aa0189315717ccf7b452ab4fc4848c717689
JavierLete <JavierLete@PC-JAVIER>
2026-04-16 17:15:14 +0200

    Controladores: PublicoController con acceso GET de /

---
+++ tiendajakarta/src/main/java/com/ipartek/formacion/ejemplos/tiendajakarta/controladores/PublicoController.java
@@ -0,0 +1,18 @@
+package com.ipartek.formacion.ejemplos.tiendajakarta.controladores;
+
+import com.ipartek.formacion.ejemplos.tiendajakarta.accesodatos.DaoProducto;
+
+import bibliotecas.controladorfrontal.ControladorFrontalServlet.Datos;
+import bibliotecas.controladorfrontal.Ruta;
+import bibliotecas.fabrica.Fabrica;
+
+public class PublicoController {
+	private static final DaoProducto DAO = (DaoProducto) Fabrica.getObjeto("dao.producto");
+	
+	@Ruta("/")
+	public static String listadoProductos(Datos datos) {
+		datos.salida().put("productos", DAO.obtenerTodos());
+		
+		return "listado-productos";
+	}
+}
