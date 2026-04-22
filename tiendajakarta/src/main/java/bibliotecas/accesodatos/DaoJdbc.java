d37cd93c71de887f94223d8658983fb39e7c8f44
JavierLete <JavierLete@host.docker.internal>
2026-04-22 17:05:14 +0200

    Mejora de documentación de DaoJdbc de la biblioteca

--- tiendajakarta/src/main/java/bibliotecas/accesodatos/DaoJdbc.java
+++ tiendajakarta/src/main/java/bibliotecas/accesodatos/DaoJdbc.java
@@ -5,8 +5,26 @@ import java.util.function.Function;
 
 public interface DaoJdbc {
 
+	/**
+	 * Ejecuta una sentencia SQL y devuelve UN registro mapeado al objeto que le corresponde
+	 * @param <T>		Tipo de los objetos que representan cada fila de la SQL
+	 * @param sql		Sentencia SQL a ejecutar
+	 * @param mapeador	Función que recibiendo una fila en un ResultSet devuelve un objeto de tipo T
+	 * @param objeto	Objeto que se va a insertar o modificar
+	 * @param args		Argumentos ordenados de la sentencia que se va a ejecutar
+	 * @return 			Objeto de tipo T o null
+	 */
 	<T> T ejecutarSqlUno(String sql, Function<ResultSet, T> mapeador, T objeto, Object... args);
 
+	/**
+	 * Ejecuta una sentencia SQL y devuelve los registros mapeados a objetos
+	 * @param <T>		Tipo de los objetos que representan cada fila de la SQL
+	 * @param sql		Sentencia SQL a ejecutar
+	 * @param mapeador	Función que recibiendo una fila en un ResultSet devuelve un objeto de tipo T
+	 * @param objeto	Objeto que se va a insertar o modificar
+	 * @param args		Argumentos ordenados de la sentencia que se va a ejecutar
+	 * @return 			Iterable de objetos del tipo T. En el caso de que no haya ninguna fila, no valdrá null, sino que será un Iterable de 0 elementos
+	 */
 	<T> Iterable<T> ejecutarSql(String sql, Function<ResultSet, T> mapeador, T objeto, Object... args);
 
 }
