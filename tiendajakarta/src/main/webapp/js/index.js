b81c2360fdb96dcf57a91991869781a840f8212a
JavierLete <JavierLete@host.docker.internal>
2026-04-28 17:12:12 +0200

    Versión con POST de productos

--- tiendajakarta/src/main/webapp/js/index.js
+++ tiendajakarta/src/main/webapp/js/index.js
@@ -1,22 +1,55 @@
 'use strict';
 
+const URL = 'http://localhost:8080/tiendajakarta/api/v1/productos';
+
 window.addEventListener('DOMContentLoaded', async () => {
-	console.log('cargado');
-	
-	const respuesta = await fetch('http://localhost:8080/tiendajakarta/api/v1/productos');
-	const productos = await respuesta.json();
-	
-	console.log(productos);
-	
-	const ul = document.querySelector('ul');
-	
-	for(const p of productos) {
-		console.log(p);
+    console.log('cargado');
+
+    await recargarLista();
+
+    const form = document.querySelector('form');
+
+	form.addEventListener('submit', async e => {
+		e.preventDefault();
 		
-		const li = document.createElement('li');
+	    const producto = { nombre: form.nombre.value, precio: form.precio.value, descripcion: form.descripcion.value };
+
+		console.log(producto);
 		
-		li.innerText = `${p.nombre}: ${p.precio}`;
+		const respuesta = await fetch(URL, {
+			method: 'POST',
+			body: JSON.stringify(producto),
+			headers: { 
+				'Content-type': 'application/json' 
+			},
+		});
 		
-		ul.appendChild(li);
-	}
+		const objeto = await respuesta.json();
+		
+		console.log(objeto);
+		
+		await recargarLista();
+	});
+	
 });
+
+async function recargarLista() {
+    const respuesta = await fetch(URL);
+    const productos = await respuesta.json();
+
+    console.log(productos);
+
+    const ul = document.querySelector('ul');
+
+	ul.innerHTML = '';
+	
+    for (const p of productos) {
+        console.log(p);
+
+        const li = document.createElement('li');
+
+        li.innerText = `${p.nombre}: ${p.precio}`;
+
+        ul.appendChild(li);
+    }
+}
