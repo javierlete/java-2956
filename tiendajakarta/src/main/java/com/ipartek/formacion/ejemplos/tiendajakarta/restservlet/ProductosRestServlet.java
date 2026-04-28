c16f04906c48e4df2ec82db6e6a8123de0b62d6f
JavierLete <JavierLete@host.docker.internal>
2026-04-28 16:12:28 +0200

    ProductosRestServlet tiene las cinco operaciones del DAO

--- tiendajakarta/src/main/java/com/ipartek/formacion/ejemplos/tiendajakarta/restservlet/ProductosRestServlet.java
+++ tiendajakarta/src/main/java/com/ipartek/formacion/ejemplos/tiendajakarta/restservlet/ProductosRestServlet.java
@@ -23,25 +23,51 @@ public class ProductosRestServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("application/json");
-		response.getWriter().append(gson.toJson(DAO_PRODUCTO.obtenerTodos()));
+		
+		Long id = obtenerId(request);
+		
+		String respuesta;
+		
+		if(id != null) {
+			respuesta = gson.toJson(DAO_PRODUCTO.obtenerPorId(id));
+		} else {
+			respuesta = gson.toJson(DAO_PRODUCTO.obtenerTodos());
+		}
+		
+		response.getWriter().append(respuesta);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		
 		Producto producto = gson.fromJson(request.getReader(), Producto.class);
-		System.out.println(producto);
+
+		response.getWriter().append(gson.toJson(DAO_PRODUCTO.insertar(producto)));
 	}
 
 	@Override
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+
 		Producto producto = gson.fromJson(request.getReader(), Producto.class);
-		System.out.println(producto);
 		
+		response.getWriter().append(gson.toJson(DAO_PRODUCTO.modificar(producto)));
 	}
 	
 	@Override
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DAO_PRODUCTO.borrar(obtenerId(request));
+	}
+
+	private static Long obtenerId(HttpServletRequest request) {
+		String ruta = request.getPathInfo();
 		
+		if(ruta == null) {
+			return null;
+		}
+		
+		return Long.parseLong(ruta.substring(1));
 	}
 	
 }
