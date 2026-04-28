5a50270bd28a33a07ef5df82b7768d661d33810f
JavierLete <JavierLete@host.docker.internal>
2026-04-28 16:21:23 +0200

    Servicio REST con diferentes estados

--- tiendajakarta/src/main/java/com/ipartek/formacion/ejemplos/tiendajakarta/restservlet/ProductosRestServlet.java
+++ tiendajakarta/src/main/java/com/ipartek/formacion/ejemplos/tiendajakarta/restservlet/ProductosRestServlet.java
@@ -19,55 +19,72 @@ public class ProductosRestServlet extends HttpServlet {
 
 	private static final DaoProducto DAO_PRODUCTO = (DaoProducto) Fabrica.getObjeto("dao.producto");
 	private static final Gson gson = new Gson();
-	
+
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("application/json");
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		Long id = obtenerId(request);
-		
+
 		String respuesta;
-		
-		if(id != null) {
-			respuesta = gson.toJson(DAO_PRODUCTO.obtenerPorId(id));
+
+		if (id != null) {
+			Producto producto = DAO_PRODUCTO.obtenerPorId(id);
+
+			if (producto == null) {
+				response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+				return;
+			}
+
+			respuesta = gson.toJson(producto);
 		} else {
 			respuesta = gson.toJson(DAO_PRODUCTO.obtenerTodos());
 		}
-		
+
+		response.setContentType("application/json");
 		response.getWriter().append(respuesta);
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("application/json");
-		
+
 		Producto producto = gson.fromJson(request.getReader(), Producto.class);
 
+		response.setStatus(HttpServletResponse.SC_CREATED);
 		response.getWriter().append(gson.toJson(DAO_PRODUCTO.insertar(producto)));
 	}
 
 	@Override
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("application/json");
-
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		Producto producto = gson.fromJson(request.getReader(), Producto.class);
-		
+
+		if(obtenerId(request) != producto.getId()) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+
+		response.setContentType("application/json");
 		response.getWriter().append(gson.toJson(DAO_PRODUCTO.modificar(producto)));
 	}
-	
+
 	@Override
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		DAO_PRODUCTO.borrar(obtenerId(request));
+		
+		response.setStatus(HttpServletResponse.SC_NO_CONTENT);
 	}
 
 	private static Long obtenerId(HttpServletRequest request) {
 		String ruta = request.getPathInfo();
-		
-		if(ruta == null) {
+
+		if (ruta == null) {
 			return null;
 		}
-		
+
 		return Long.parseLong(ruta.substring(1));
 	}
-	
+
 }
