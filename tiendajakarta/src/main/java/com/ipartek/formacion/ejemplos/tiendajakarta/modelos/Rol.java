1216266e9d6c923655ca66da92103d0898e721c1
JavierLete <JavierLete@host.docker.internal>
2026-04-30 17:23:09 +0200

    Adaptación de la clase Rol para que sea una entidad persistente

--- tiendajakarta/src/main/java/com/ipartek/formacion/ejemplos/tiendajakarta/modelos/Rol.java
+++ tiendajakarta/src/main/java/com/ipartek/formacion/ejemplos/tiendajakarta/modelos/Rol.java
@@ -1,6 +1,11 @@
 package com.ipartek.formacion.ejemplos.tiendajakarta.modelos;
 
 import bibliotecas.accesodatos.Identificable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +15,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+
+@Entity
+@Table(name = "roles")
 public class Rol implements Identificable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombre;
 	private String descripcion;
