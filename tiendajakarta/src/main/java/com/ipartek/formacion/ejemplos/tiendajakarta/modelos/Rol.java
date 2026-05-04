252d0fd0fa9a8ea55897386fbd64306b357b3432
JavierLete <JavierLete@host.docker.internal>
2026-05-04 16:33:36 +0200

    Uso de Hibernate Validator como implementación de Jakarta Validation

--- tiendajakarta/src/main/java/com/ipartek/formacion/ejemplos/tiendajakarta/modelos/Rol.java
+++ tiendajakarta/src/main/java/com/ipartek/formacion/ejemplos/tiendajakarta/modelos/Rol.java
@@ -1,11 +1,15 @@
 package com.ipartek.formacion.ejemplos.tiendajakarta.modelos;
 
 import bibliotecas.accesodatos.Identificable;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,6 +26,13 @@ public class Rol implements Identificable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@NotBlank
+	@Size(max = 20)
+	@Column(unique = true)
 	private String nombre;
+
+	@Lob
+	@Size(max = 2000)
 	private String descripcion;
 }
