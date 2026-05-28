7222a5d05a70067fa5bf406614e117327fff5327
Javier <javier@eskura21.com>
2026-05-28 16:11:16 +0200

    Las tareas las obtenemos del json-server

--- react/tareas/src/App.jsx
+++ react/tareas/src/App.jsx
@@ -1,19 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import ListadoTareas from './ListadoTareas'
 import { TareaNueva } from './TareaNueva'
 import { Titulo } from './Titulo'
+import { URL_TAREAS } from './constantes'
 
 function App() {
   const [tareas, setTareas] = useState([]);
-  
+
+  useEffect(() => {
+    async function fetchData() {
+      // You can await here
+      const respuesta = await fetch(URL_TAREAS);
+      const listado = await respuesta.json()
+      setTareas(listado.map(tarea => tarea.texto).reverse());
+    }
+    fetchData();
+  }, []); // Or [] if effect doesn't need props 
+
   return (
     <>
       <Titulo texto="Tareas" />
 
-      <TareaNueva tareas={tareas} setTareas={setTareas}/>
+      <TareaNueva tareas={tareas} setTareas={setTareas} />
 
-      <ListadoTareas tareas={tareas}/>
+      <ListadoTareas tareas={tareas} />
     </>
   )
 }
---
+++ react/tareas/src/constantes.jsx
@@ -0,0 +1,1 @@
+export const URL_TAREAS = 'http://localhost:3000/tareas/'
