a61c938bee2310bff62539dd69c7710230a0298d
'Javier <'javier@eskura21.com'>
2026-06-08 16:03:59 +0200

    ipartexpring: Spring Starter Project (JPA, Lombok, Validation)

---
+++ ipartexpring/src/main/java/com/ipartek/formacion/ejemplos/ipartexpring/IpartexpringApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.ejemplos.ipartexpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpartexpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpartexpringApplication.class, args);
+	}
+
+}
---
+++ ipartexpring/src/test/java/com/ipartek/formacion/ejemplos/ipartexpring/IpartexpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.ejemplos.ipartexpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpartexpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
