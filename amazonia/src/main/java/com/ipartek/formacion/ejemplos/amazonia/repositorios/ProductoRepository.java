2520ef35b2d807f6064cb2bfb1fef4fa14b3492d
'Javier <'javier@eskura21.com'>
2026-06-15 16:26:35 +0200

    Repositorios y servicios REST automáticos con HAL Explorer

---
+++ amazonia/src/main/java/com/ipartek/formacion/ejemplos/amazonia/repositorios/ProductoRepository.java
@@ -0,0 +1,11 @@
+package com.ipartek.formacion.ejemplos.amazonia.repositorios;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.ipartek.formacion.ejemplos.amazonia.entidades.Producto;
+
+@RepositoryRestResource(path = "productos", collectionResourceRel = "productos")
+public interface ProductoRepository extends CrudRepository<Producto, Long> {
+
+}
