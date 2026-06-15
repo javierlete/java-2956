86d7fb8077031ff09d1d726d7c374f8ea0a9d535
'Javier <'javier@eskura21.com'>
2026-06-15 15:51:46 +0200

    amazonia: inicio proyecto

---
+++ amazonia/src/test/java/com/ipartek/formacion/ejemplos/amazonia/AmazoniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.ejemplos.amazonia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazoniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
