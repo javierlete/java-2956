2c8a30ce225279468acbb0256ae04b4889a0ebb5
'Javier <'javier@eskura21.com'>
2026-06-16 16:24:16 +0200

    Controladores y vistas

---
+++ amazonia/src/main/java/com/ipartek/formacion/ejemplos/amazonia/controladores/IndexController.java
@@ -0,0 +1,31 @@
+package com.ipartek.formacion.ejemplos.amazonia.controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ipartek.formacion.ejemplos.amazonia.servicios.AnonimoService;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+	@Autowired
+	private AnonimoService anonimoService;
+	
+	@GetMapping({"/", "productos"})
+	public String productos(Model modelo) {
+		modelo.addAttribute("productos", anonimoService.listarProductos());
+		
+		return "productos";
+	}
+
+	@GetMapping({"productos/{id}"})
+	public String producto(@PathVariable Long id, Model modelo) {
+		modelo.addAttribute("producto", anonimoService.obtenerProductoPorId(id).get());
+		
+		return "producto";
+	}
+}
