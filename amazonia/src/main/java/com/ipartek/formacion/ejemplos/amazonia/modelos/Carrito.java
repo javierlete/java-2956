2539ee8a1c6ae8908077558d08e13fecddea3b2f
'Javier <'javier@eskura21.com'>
2026-07-07 16:42:31 +0200

    Carrito con información de totales e IVAs

--- amazonia/src/main/java/com/ipartek/formacion/ejemplos/amazonia/modelos/Carrito.java
+++ amazonia/src/main/java/com/ipartek/formacion/ejemplos/amazonia/modelos/Carrito.java
@@ -1,5 +1,6 @@
 package com.ipartek.formacion.ejemplos.amazonia.modelos;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,13 +18,36 @@ import lombok.ToString;
 @Component
 @ToString
 public class Carrito {
+	private static final BigDecimal IVA = new BigDecimal("0.21");
+
 	@Getter
 	private final List<Linea> lineas = new ArrayList<>();
+	
+	public BigDecimal getTotal() {
+		return lineas.stream().map(linea -> linea.getTotal()).reduce(BigDecimal.ZERO, (total, acumulado) -> total.add(acumulado));
+	}
+	
+	public BigDecimal getIva() {
+		return getTotal().multiply(IVA);
+	}
 	
 	@Data
 	@Builder
 	public static class Linea {
+		
 		private Producto producto;
 		private Integer cantidad;
+		
+		public BigDecimal getSubTotal() {
+			return producto.getPrecio().multiply(new BigDecimal(cantidad));
+		}
+		
+		public BigDecimal getIva() {
+			return getSubTotal().multiply(IVA);
+		}
+		
+		public BigDecimal getTotal() {
+			return getSubTotal();
+		}
 	}
 }
