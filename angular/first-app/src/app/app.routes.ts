78cf0c261d9563892105b747b432f87a7f320d5e
Javier <javier@eskura21.com>
2026-07-16 17:17:36 +0200

    Rutado Home y Details

--- angular/first-app/src/app/app.routes.ts
+++ angular/first-app/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Details } from './details/details';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+        title: 'Home page',
+    },
+    {
+        path: 'details/:id',
+        component: Details,
+        title: 'Home details',
+    },
+];
