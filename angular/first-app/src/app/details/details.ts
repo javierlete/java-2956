d580058a25282a70f484f50603a4cc6b7d54c2c1
Javier <javier@eskura21.com>
2026-07-20 16:35:22 +0200

    Formulario para contacto

--- angular/first-app/src/app/details/details.ts
+++ angular/first-app/src/app/details/details.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { HousingLocationInfo } from '../housinglocation';
 import { ActivatedRoute } from '@angular/router';
 import { Housing } from '../housing';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './details.html',
   styleUrl: './details.css',
 })
@@ -15,8 +16,22 @@ export class Details {
 
   housingLocation?: HousingLocationInfo;
 
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
   }
 }
