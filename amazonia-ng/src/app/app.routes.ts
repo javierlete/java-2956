3b1ac75f206b5965bb5e07e1eface30ee105b173
Javier <javier@eskura21.com>
2026-07-21 16:04:26 +0200

    Visualización del componente a través del router-outlet

--- amazonia-ng/src/app/app.routes.ts
+++ amazonia-ng/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FichasProductos } from './fichas-productos/fichas-productos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: FichasProductos },
+];
