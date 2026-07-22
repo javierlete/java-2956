6c529dcd6fe27f005a9be20e63e4707b284f6cc8
Javier <javier@eskura21.com>
2026-07-22 16:01:29 +0200

    Precios en euros

--- amazonia-ng/src/app/fichas-productos/fichas-productos.ts
+++ amazonia-ng/src/app/fichas-productos/fichas-productos.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { Producto } from '../producto';
 import { ProductoService } from '../producto-service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-fichas-productos',
+  imports: [CurrencyPipe],
   templateUrl: './fichas-productos.html',
   styleUrl: './fichas-productos.css',
 })
--- amazonia-ng/src/main.ts
+++ amazonia-ng/src/main.ts
@@ -4,5 +4,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+import '@angular/common/locales/global/es';
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
