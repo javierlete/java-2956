43d0387f84e89a547a4178ee3f85aa90ad38bff9
Javier <javier@eskura21.com>
2026-07-23 17:25:27 +0200

    Botón de editar muestra los datos (FALTA LA CATEGORÍA, ahora tiene un valor por defecto)

--- amazonia-ng/src/app/administracion-formulario-producto/administracion-formulario-producto.ts
+++ amazonia-ng/src/app/administracion-formulario-producto/administracion-formulario-producto.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { LabelInput } from '../label-input/label-input';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { LabelInput, Opcion } from '../label-input/label-input';
+import { ActivatedRoute } from '@angular/router';
+import { ProductoService } from '../producto-service';
+import { Producto } from '../producto';
+import { CategoriaService } from '../categoria-service';
 
 @Component({
   selector: 'app-administracion-formulario-producto',
@@ -7,4 +11,35 @@ import { LabelInput } from '../label-input/label-input';
   templateUrl: './administracion-formulario-producto.html',
   styleUrl: './administracion-formulario-producto.css',
 })
-export class AdministracionFormularioProducto {}
+export class AdministracionFormularioProducto {
+  productoService = inject(ProductoService);
+  categoriaService = inject(CategoriaService);
+
+  changeDetectorRef = inject(ChangeDetectorRef);
+  route = inject(ActivatedRoute);
+
+  opciones: Opcion[] = [];
+
+  producto: Producto = {
+    id: 0,
+    nombre: '',
+    precio: 0,
+    categoria: '',
+    descripcion: '',
+  };
+
+  constructor() {
+    const id = Number(this.route.snapshot.params['id']);
+    
+    this.categoriaService.obtenerTodos().then(categorias => {
+      this.opciones = categorias.map(c => ({ id: String(c.id), texto: c.nombre }));
+
+      if (id) {
+        this.productoService.obtenerPorId(id).then(p => {
+          this.producto = p;
+          this.changeDetectorRef.markForCheck();
+        });
+      }
+    });
+  }
+}
