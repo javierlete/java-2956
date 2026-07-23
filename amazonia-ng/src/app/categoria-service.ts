86124f40f2b2d682d914f86bd80aad40210e76f6
Javier <javier@eskura21.com>
2026-07-23 17:17:16 +0200

    Servicio de categorías

--- amazonia-ng/src/app/categoria-service.ts
+++ amazonia-ng/src/app/categoria-service.ts
@@ -1,4 +1,14 @@
 import { Service } from '@angular/core';
+import { Categoria } from './categoria';
 
 @Service()
-export class CategoriaService {}
+export class CategoriaService {
+    private readonly URL = 'http://localhost:8080/api/v2/categorias'
+
+    async obtenerTodos(): Promise<Categoria[]> {
+        const respuesta = await fetch(this.URL);
+        const categorias = await respuesta.json();
+
+        return Promise.resolve(categorias);
+    }
+}
