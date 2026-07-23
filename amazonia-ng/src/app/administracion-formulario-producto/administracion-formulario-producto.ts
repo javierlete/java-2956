2a765e3da6cc123ec96e1dc7d1c04d3968e8435e
Javier <javier@eskura21.com>
2026-07-23 16:31:14 +0200

    Formulario usando LabelInput

--- amazonia-ng/src/app/administracion-formulario-producto/administracion-formulario-producto.ts
+++ amazonia-ng/src/app/administracion-formulario-producto/administracion-formulario-producto.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LabelInput } from '../label-input/label-input';
 
 @Component({
   selector: 'app-administracion-formulario-producto',
-  imports: [],
+  imports: [LabelInput],
   templateUrl: './administracion-formulario-producto.html',
   styleUrl: './administracion-formulario-producto.css',
 })
