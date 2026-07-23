942eac57f576f62c0b1a47d562adc9c33344c91f
Javier <javier@eskura21.com>
2026-07-23 15:23:55 +0200

    Creamos las rutas y enlazamos el menú a administración listado productos

--- amazonia-ng/src/app/app.routes.ts
+++ amazonia-ng/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { FichasProductos } from './fichas-productos/fichas-productos';
 import { FichaProducto } from './ficha-producto/ficha-producto';
+import { AdministracionListadoProductos } from './administracion-listado-productos/administracion-listado-productos';
+import { AdministracionFormularioProducto } from './administracion-formulario-producto/administracion-formulario-producto';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: '/productos' },
     { path: 'productos', component: FichasProductos },
     { path: 'productos/:id', component: FichaProducto },
+    { path: 'admin/productos', component: AdministracionListadoProductos },
+    { path: 'admin/productos/:id', component: AdministracionFormularioProducto },
 ];
